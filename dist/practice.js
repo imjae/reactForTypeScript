915ecad4ec91a1aef8423f81e24fe2763ec0d428
namu5 <code@kakao.com>
2021-02-26 10:39:52 +0900

    기본 타입

---
+++ dist/practice.js
@@ -0,0 +1,14 @@
+"use strict";
+var count = 0;
+count += 1;
+count = '갑자기 분위기 문자열';
+var message = 'hello world';
+var doen = true;
+var numbers = [1, 2, 3];
+var messages = ['hello', 'world'];
+messages.push(1);
+var mightBeUndefined = undefined;
+var nullableNumber = null;
+var color = 'red';
+color = 'yellow';
+color = 'green';
