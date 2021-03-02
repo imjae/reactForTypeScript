788d09705d58d82acb5e6251ebccad217ad9ecbd
imjae <code@kakao.com>
2021-03-02 09:44:48 +0900

    type으로 optional, function 주고받기

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Greetings from './Greetings';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Greetings from "./Greetings";
 
- const App: React.FC = () => {
-  return <Greetings name="Hello" />;
-}
+const App: React.FC = () => {
+  const onClick = (name: string) => {
+    console.log(`${name} says Hello`);
+  };
+  return <Greetings name="Hello" onClick={onClick} />;
+};
 
 export default App;
