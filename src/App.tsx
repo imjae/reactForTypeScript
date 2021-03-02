0539579be5832259be379aaa3d7f27dbb1e8a8f2
imjae <code@kakao.com>
2021-03-02 11:30:45 +0900

    Generics 로 리액트 상태 관리하기

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Greetings from "./Greetings";
+import Counter from "./Counter";
 
 const App: React.FC = () => {
-  const onClick = (name: string) => {
-    console.log(`${name} says Hello`);
-  };
-  return <Greetings name="Hello" onClick={onClick} />;
+  return <Counter />
 };
 
 export default App;
