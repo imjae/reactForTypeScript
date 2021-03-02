4192b35c9af4786bf8acc29c3f3605b4708776e7
imjae <code@kakao.com>
2021-03-02 11:45:42 +0900

    인풋상태 관리하기

--- src/MyForm.tsx
+++ src/MyForm.tsx
@@ -15,17 +15,28 @@ function MyForm({ onSubmit }: MyFormProps) {
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     // e 값을 무엇으로 설정해야할까 ?
     // 일단 모를때는 any로 설정한다.
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     //여기도 모르니까 any로 준다.
+    e.preventDefault();
+    onSubmit(form);
+    setForm({
+      name: "",
+      description: "",
+    });
   };
 
   return (
     <form onSubmit={handleSubmit}>
-        <input name="name" value={name} onChange={onChange} />
-        <input name="description" value={description} onChange={onChange} />
-        <button type ="submit">등록</button>
+      <input name="name" value={name} onChange={onChange} />
+      <input name="description" value={description} onChange={onChange} />
+      <button type="submit">등록</button>
     </form>
   );
 }
