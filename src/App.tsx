145ff5130aae07300c3535d255860135fbe2e08d
imjae <code@kakao.com>
2021-03-02 20:05:12 +0900

    useReducer 사용한 sample컴포넌트 작성

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,13 @@ import Greetings from "./Greetings";
 import Counter from "./Counter";
 import MyForm from "./MyForm";
 import Counter2 from "./Counter2";
+import ReducerSample from "./ReducerSample";
 
 const App: React.FC = () => {
   const onSubmit = (form: { name: string; description: string }) => {
     console.log(form);
   };
-  return <Counter2 />;
+  return <ReducerSample />;
 };
 
 export default App;
